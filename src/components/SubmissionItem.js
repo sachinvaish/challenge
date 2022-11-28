3af86e86c3efee9ea578f5f1c693d39ebd99ab2a
sachinvaish <vaish139@gmail.com>
2022-11-28 08:25:01 +0530

    minor fixtures

--- src/components/SubmissionItem.js
+++ src/components/SubmissionItem.js
@@ -1,8 +1,9 @@
-import { Card, CardContent, CardMedia, Typography, CardHeader, CardActions, Avatar, Button, Box, IconButton } from '@mui/material';
+import { Link, Card, CardMedia, Typography, CardHeader, CardActions, Avatar, Button, Box, IconButton } from '@mui/material';
 import ThumbUpIcon from '@mui/icons-material/ThumbUp';
-import React from 'react';
+import React, { useRef } from 'react';
 
 export default function SubmissionItem() {
+  
     return (
         <Card sx={{ width: 350, color: 'black', margin: 1 }}>
             <CardMedia
@@ -18,12 +19,16 @@ export default function SubmissionItem() {
                             R
                         </Avatar>
                     }
-                    title="Sachin Vaish"
+                    title={
+                        <Link variant="h6" onClick={()=>{alert('taking you to user profile');}} sx={{cursor:'pointer',textDecoration:'none', color : 'black'}} >
+                           Sachin Vaish
+                        </Link>
+                     }
                     subheader="7 Feedbacks"
                 />
                 <CardActions>
                 <Typography variant='h6'>23</Typography>
-                    <IconButton size="small">
+                    <IconButton size="small" onClick={()=>{alert('UPVOTE');}}>
                         <ThumbUpIcon />
                     </IconButton>
                 </CardActions>
--- src/components/Submissions.js
+++ src/components/Submissions.js
@@ -1,11 +1,15 @@
-import { Avatar, Container, Typography } from '@mui/material';
+import {Container } from '@mui/material';
 import { Stack} from '@mui/system';
 import React from 'react';
 import SubmissionItem from './SubmissionItem';
 
+const getSubmissions=()=>{
+
+}
+
 export default function Submissions() {
   return (
-    <Container sx={{marginTop : '50px'}}>
+    <Container sx={{marginTop : '50px'}} >
       <Stack direction='row' sx={{display:'flex', flexWrap:'wrap'}}>
         <SubmissionItem/>
         <SubmissionItem/>
